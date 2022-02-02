6b5d0f9c2a04a1f661c5da9cedd172ff082bb544
F-bh <72809273+F-bh@users.noreply.github.com>
2022-02-02 14:25:29 +0100

    file name and compression are now saved in the code
    - user can choose a new name for the file
    - the link now contains the information that the file was compressed (only when compression is enabled)

--- src/components/editor.svelte
+++ src/components/editor.svelte
@@ -19,17 +19,19 @@
   }
 
   const URL: string = "https://quri.de/";
+  const URLSIZE: number = new Blob([URL]).size; 
+
   //correction level to bytes allowed
-  const MAXFILESIZES: Map<number, number> = new Map([[0, 2364], [1, 1867], [2, 1332], [3, 1002]]);
+  const MAXFILESIZES: Map<number, number> = new Map([[0, 2364 - URLSIZE], [1, 1867 - URLSIZE], [2, 1332 - URLSIZE], [3, 1002 - URLSIZE]]);
 
   let files: FileList;
   let errorMessage: string = null;
   let isDropHover: boolean = false;
   let corrLvl: number = 0;
-  
   let compressionEnabled: boolean = true;
   let loading = false;
   let imgUrl = null;
+  let fileName = null;
 
   function reset(){
     errorMessage = null;
@@ -37,12 +39,14 @@
     isDropHover = false;
     files = null;
     loading = false;
+    fileName = null;
   }
   
   $:{
     corrLvl;
     compressionEnabled;
     files;
+    fileName;
     if (files != null && errorMessage == null){
       generateUrl();
     }
@@ -69,8 +73,12 @@
 
       let code = qrcode(0, lvl);
       try{
-        let data = await getData();
-        code.addData(URL + data);
+        let [data,filename] = await getData();
+        if (fileName == null){
+          fileName = filename;
+        }
+        
+        code.addData(compressionEnabled ? `${URL}c/${data}/${fileName}` : `${URL}${data}/${fileName}`);
       }
       catch(e){
         let msg = e["msg"]
@@ -80,7 +88,7 @@
             reset();
             break;
           case DataExceptionMsg.size :
-            errorMessage = `🛑🛑\nThe given file is too large.\nYou could try enabling or disabling compression and\nreducing the correction level.\nYour filesize: ${e["val"]}kb\nmax. allowed filesize: ${MAXFILESIZES.get(corrLvl)}kb\n🛑🛑`
+            errorMessage = `🛑🛑\nThe given file is too large.\nYou could try enabling or disabling compression\nand reducing the correction level.\nYour filesize: ${e["val"]}kb\nmax. allowed filesize: ${MAXFILESIZES.get(corrLvl)}kb\n🛑🛑`
             break;
         }
         loading = false;
@@ -91,7 +99,7 @@
       }
       catch(e){
         let sizesInBytes = e.slice(23, e.length-2).split('>');
-        errorMessage = `🛑🛑\nThe given file is too large.\nYou could try enabling or disabling compression and\nreducing the correction level.\nYour filesize: ${sizesInBytes[0]/1000}kb\nmax. allowed filesize: ${sizesInBytes[1]/1000}kb\n🛑🛑`
+        errorMessage = `🛑🛑\nThe given file is too large.\nYou could try enabling or disabling compression and\nreducing the correction level.\nYour filesize: ${sizesInBytes[0]/10000}kb\nmax. allowed filesize: ${sizesInBytes[1]/1000}kb\n🛑🛑`
         loading = false;
         return;
       }
@@ -100,16 +108,16 @@
       imgUrl = code.createDataURL();
     }
 
-  //TODO save fileType information
-  function getData(): Promise<string>{
+  function getData(): Promise<[data: string, filename:string]>{
     if (files && files.length == 1){ 
       let reader = new FileReader();
+      let name = files[0].name;
       if (compressionEnabled == true){
         return new Promise ((resolve, _) => {
           reader.onload = () => {
             let buf: Uint8Array = new Uint8Array(reader.result as ArrayBuffer); //TODO inform user an error has occured
             let compressed: Uint8Array = pako.deflate(buf);
-            resolve(btoa(compressed.toString()));
+            resolve([btoa(compressed.toString()), name]);
           }
           reader.readAsArrayBuffer(files.item(0));
         });
@@ -122,9 +130,9 @@
         else{
           return new Promise((resolve, _) => {
             reader.onload = () =>{
-              resolve(btoa(reader.result as string));
+              resolve([btoa(reader.result as string), name]);
             }
-            reader.readAsBinaryString(files.item(0));
+          reader.readAsBinaryString(files.item(0));
           });
         }
       }
@@ -180,6 +188,7 @@
   {/if}
     
   <div class="bg-zinc-100 rounded-md drop-shadow-xl flex flex-col p-5">
+    <p class="font-extrabold m-3 text-center text-xl"> settings </p>
     <div class="bg-zinc-200 rounded-md p-2">
       <p class="font-medium text-center" title="determins the amount of space used for error correction higher = less data"> current correction level: { corrLvl } </p>
       <div class="flex flex-row justify-center">
@@ -195,6 +204,15 @@
           <p class="text-xs font-light"> may increase the size of very small files </p>
         </div>
         <input class="p-3 m-5 h-5 w-5 justify-self-end self-center" type="checkbox" bind:checked={ compressionEnabled }>
-    </div> 
+    </div>
+    
+    {#if fileName != null}
+      <div class="bg-zinc-200 rounded-md p-2 mt-3">
+        <p class="font-medium text-center" title="determins the amount of space used for the file name, shorter = less data"> current size of the file name: { new Blob([fileName]).size } bytes </p>
+        <div class="flex flex-row justify-center">
+          <input bind:value={fileName} type="text" class="mt-3 bg-slate-100 rounded-md p-1">
+        </div>
+      </div>
+    {/if}
   </div>
 </div>
